135cd2c9e389370389cd77f204da03bf720db3ac
Juan Gomez <jgomez@quenchwater.com>
2022-03-14 11:56:23 -0400

    added cors middleware

--- app/app/settings.py
+++ app/app/settings.py
@@ -89,6 +89,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'simple_history.middleware.HistoryRequestMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
